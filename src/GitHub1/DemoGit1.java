0a60ba5c0a726dcf6c764469b059cd587cced990
sukhvirkaur <sukhvirkaur@192.168.0.24>
2023-07-29 10:46:15 -0400

    Demo1

---
+++ src/GitHub1/DemoGit1.java
@@ -0,0 +1,10 @@
+package GitHub1;
+
+public class DemoGit1 {
+	public static void main (String args[]) {
+		
+	
+	System.out.println("Hello .....");
+	System.out.println("What are you doing");
+	}
+}
